2a32457ea5c6627d93e345eebdbf8c2d6c12fb8a
WaifuAnton <WaifuAnton@users.noreply.github.com>
2019-11-02 19:29:40 +0200

    fall hitting

--- Assets/Scripts/DinoMovement.cs
+++ Assets/Scripts/DinoMovement.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class DinoMovement : MonoBehaviour
 {
     public float speed = 10;
-    public float gravity = -9.81f;
+    public float gravity = -.81f;
     public float acceleration = 5;
     Rigidbody2D rb2d;
     Vector2 move, fall;
+    RaycastHit2D[] hitBuffer = new RaycastHit2D[1];
+    const float minMoveDistance = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +22,19 @@ public class DinoMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Fall();
+        //int count = rb2d.Cast(fall, contactFilter, hitBuffer, distance + shellRadius);
+        rb2d.Cast(Vector2.down, hitBuffer);
+        {
+            float distance = hitBuffer[0].distance;
+            if (distance > minMoveDistance)
+                Fall();
+        }
     }
 
     void Fall()
     {
         transform.Translate(fall);
         fall += Physics2D.gravity * Time.fixedDeltaTime;
-        fall = Vector2.ClampMagnitude(fall, 50 * Time.fixedDeltaTime);
+        fall = Vector2.ClampMagnitude(fall, 90 * Time.fixedDeltaTime);
     }
 }
