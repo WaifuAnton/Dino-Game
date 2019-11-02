4c093a3d5dbf82b000f0f6fb24d2d91b5eab95f0
WaifuAnton <WaifuAnton@users.noreply.github.com>
2019-11-02 18:31:02 +0200

    falling function

--- Assets/Scripts/DinoMovement.cs
+++ Assets/Scripts/DinoMovement.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class DinoMovement : MonoBehaviour
 {
     public float speed = 10;
+    public float gravity = -9.81f;
+    public float acceleration = 5;
+    Rigidbody2D rb2d;
+    Vector2 move, fall;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        fall = Physics2D.gravity * Time.fixedDeltaTime;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(Vector2.right * speed * Time.fixedDeltaTime);
+        Fall();
+    }
+
+    void Fall()
+    {
+        transform.Translate(fall);
+        fall += Physics2D.gravity * Time.fixedDeltaTime;
+        fall = Vector2.ClampMagnitude(fall, 50 * Time.fixedDeltaTime);
     }
 }
