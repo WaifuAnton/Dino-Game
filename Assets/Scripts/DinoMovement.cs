12bb981c17632420c5f5b3cc9ad23056d9a7dbb3
WaifuAnton <WaifuAnton@users.noreply.github.com>
2019-11-08 23:19:00 +0200

    normal movement (no jumps)

--- Assets/Scripts/DinoMovement.cs
+++ Assets/Scripts/DinoMovement.cs
@@ -5,36 +5,29 @@ using UnityEngine;
 public class DinoMovement : MonoBehaviour
 {
     public float speed = 10;
-    public float gravity = -.81f;
-    public float acceleration = 5;
     Rigidbody2D rb2d;
-    Vector2 move, fall;
+    Vector2 move;
     RaycastHit2D[] hitBuffer = new RaycastHit2D[1];
-    const float minMoveDistance = 0.5f;
+    const float minMoveDistance = 0.4f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        fall = Physics2D.gravity * Time.fixedDeltaTime;
+        move = new Vector2(speed, 0);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //int count = rb2d.Cast(fall, contactFilter, hitBuffer, distance + shellRadius);
         rb2d.Cast(Vector2.down, hitBuffer);
+        float distance = hitBuffer[0].distance;
+        if (distance < minMoveDistance)
         {
-            float distance = hitBuffer[0].distance;
-            if (distance > minMoveDistance)
-                Fall();
+            rb2d.gravityScale = 0;
+            rb2d.AddForce(move, ForceMode2D.Force);
         }
-    }
-
-    void Fall()
-    {
-        transform.Translate(fall);
-        fall += Physics2D.gravity * Time.fixedDeltaTime;
-        fall = Vector2.ClampMagnitude(fall, 90 * Time.fixedDeltaTime);
+        else
+            rb2d.gravityScale = 1;
     }
 }
