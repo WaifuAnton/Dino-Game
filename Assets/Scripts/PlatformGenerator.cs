86330b07b121dd7e0180dff35a2d31831ec71f2c
WaifuAnton <WaifuAnton@users.noreply.github.com>
2019-11-09 16:39:52 +0200

    platform spawner

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour
 {
+    GameObject platform;
+    const float modifier = 1100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        platform = transform.parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        DinoMovement dino = collision.GetComponent<DinoMovement>();
+        if (dino != null)
+        {
+            Instantiate(platform, new Vector3(platform.transform.position.x + modifier,
+                platform.transform.position.y, platform.transform.position.z + 1), Quaternion.identity);
+            Destroy(transform.parent.gameObject);
+        }
     }
 }
