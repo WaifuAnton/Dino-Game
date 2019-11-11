ad9364558955567ac70d473600c9a61085362ee0
WaifuAnton <WaifuAnton@users.noreply.github.com>
2019-11-11 23:18:09 +0200

    started to work on cactus connection with canvas

--- Assets/Scripts/CactusHit.cs
+++ Assets/Scripts/CactusHit.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CactusHit : MonoBehaviour
 {
+    [SerializeField] Text gameOverText;
+    [SerializeField] CinemachineVirtualCamera cinemachine;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +27,10 @@ public class CactusHit : MonoBehaviour
         if (Dino != null) 
         {           
             Rigidbody2D rb2d = Dino.GetComponent<Rigidbody2D>();
+            cinemachine.enabled = false;
             Dino.enabled = false;
             rb2d.Sleep();
+            gameOverText.enabled = true;         
         }
     }
 }
